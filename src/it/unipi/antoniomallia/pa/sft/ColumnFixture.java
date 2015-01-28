4793436cf0d0630e8557f772383572ebed449e1c
amallia <me@antoniomallia.it>
2015-01-28 23:43:04 +0100

    Convertirore di tipo e implementazione della columnfixture

--- src/it/unipi/antoniomallia/pa/sft/ColumnFixture.java
+++ src/it/unipi/antoniomallia/pa/sft/ColumnFixture.java
@@ -1,13 +1,26 @@
 package it.unipi.antoniomallia.pa.sft;
 
+import it.unipi.antoniomallia.pa.generator.HTMLGenerator;
+import it.unipi.antoniomallia.pa.model.Cell.Colour;
+import it.unipi.antoniomallia.pa.model.Row;
 import it.unipi.antoniomallia.pa.model.Table;
 
-public class ColumnFixture implements Fixture{
+public abstract class ColumnFixture implements Fixture {
+	private final static int DATA_START_ROW = 3;
+
+	public abstract boolean check(Row row);
 
 	@Override
 	public String execute(Table table) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = DATA_START_ROW; i < table.rows.size(); i++) {
+			if (check(table.rows.get(i))) {
+				table.rows.get(i).cells.get(table.rows.get(i).cells.size()-1).colour = Colour.GREEN;
+			} else {
+				table.rows.get(i).cells.get(table.rows.get(i).cells.size()-1).colour = Colour.RED;
+			}
+		}
+		// Execute tests and adjust the table
+		// Convert the table to htmlTable
+		return table.toHTML(new HTMLGenerator());
 	}
-
 }
