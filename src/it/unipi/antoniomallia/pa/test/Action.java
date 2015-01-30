e45722b88cbdf94d47cddc6eb52c1768a0eb5775
Antonio Mallia <antonio.mallia@iit.cnr.it>
2015-01-30 10:25:31 +0100

    Update git ignore. Commented the Action class

--- src/it/unipi/antoniomallia/pa/test/Action.java
+++ src/it/unipi/antoniomallia/pa/test/Action.java
@@ -2,13 +2,13 @@ package it.unipi.antoniomallia.pa.test;
 
 import it.unipi.antoniomallia.pa.sft.ActionFixture;
 
-public class Action extends ActionFixture {
-	public boolean process() {
-		Accumulator acc = new Accumulator();
-		acc.add(product(12, 12));
-		return check(sqrt(acc.add(product(7, 7))));
-	}
-	private boolean check(Object obj) {
-		return obj.equals(13.8924);
-	}
-}
+//public class Action extends ActionFixture {
+//	public boolean process() {
+//		Accumulator acc = new Accumulator();
+//		acc.add(product(12, 12));
+//		return check(sqrt(acc.add(product(7, 7))));
+//	}
+//	private boolean check(Object obj) {
+//		return obj.equals(13.8924);
+//	}
+//}
